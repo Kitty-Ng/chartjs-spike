7a5e400d4f92a9de636f29b43f22b8bf5950e6a4
Kitty Ng <kittyng89@gmail.com>
2017-11-04 13:23:01 -0500

    initial GET from db complete

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,5 +1,51 @@
-myApp.controller('InfoController', function(UserService) {
+myApp.controller('InfoController', function (UserService) {
   console.log('InfoController created');
   var vm = this;
   vm.userService = UserService;
+  vm.chartData = UserService.chartData;
+
+  vm.getChartData = function () {
+    UserService.getChartData()
+  }
+  
+  vm.mainChartYears = vm.chartData.data.reduce(function (prev, curr) {
+    var index = prev.findIndex(function (element) {
+      return element.year === curr.year;
+    });
+    if (index < 0) {
+      prev.push(curr);
+    }
+    return prev;
+  },
+    []
+  ).map(function (element) { return element.year }).map(String)
+
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: vm.mainChartYears,
+      datasets: [{
+        label: '# of MCM cases per year',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+
 });
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -1,35 +1,42 @@
-myApp.factory('UserService', function($http, $location){
-  console.log('UserService Loaded');
+myApp.service('UserService', function ($http, $location) {
+  var self = this;
 
-  var userObject = {};
+  self.userObject = {};
+  self.appliancesObj = { appliances: [] };
+  self.chartData = {data: []};
 
-  return {
-    userObject : userObject,
-
-    getuser : function(){
-      console.log('UserService -- getuser');
-      $http.get('/user').then(function(response) {
-          if(response.data.username) {
-              // user has a curret session on the server
-              userObject.userName = response.data.username;
-              console.log('UserService -- getuser -- User Data: ', userObject.userName);
-          } else {
-              console.log('UserService -- getuser -- failure');
-              // user has no session, bounce them back to the login page
-              $location.path("/home");
-          }
-      },function(response){
-        console.log('UserService -- getuser -- failure: ', response);
+  self.getuser = function () {
+    $http({
+      method: "GET",
+      url: '/user'
+    }).then(function (response) {
+      if (response.data.username) {
+        // user has a current session on the server
+        self.userObject.userName = response.data.username;
+      } else {
+        // user has no session, bounce them back to the login page
         $location.path("/home");
-      });
-    },
-
-    logout : function() {
-      console.log('UserService -- logout');
-      $http.get('/user/logout').then(function(response) {
-        console.log('UserService -- logout -- logged out');
-        $location.path("/home");
-      });
-    }
+      }
+    })
   };
+
+  self.getChartData = function () {
+    //on page load, GET all chart data from DB to the DOM
+    $http({
+      method: 'GET',
+      url: '/chart'
+    })
+    .then(function (res) {
+      self.chartData.data = res.data;
+    });
+  };
+
+  self.logout = function () {
+    $http({
+      method: "GET",
+      url: '/user/logout',
+    }).then(function (response) {
+      $location.path("/home");
+    });
+  }
 });
